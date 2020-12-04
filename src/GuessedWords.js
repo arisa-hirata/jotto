fc24dadaaf7cc3b933c20da8249787839cda0bd2
“arisa-hirata” <arisa.pirata@gmail.com>
2020-12-04 13:46:27 -0800

    Added Bootstrap styling

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -3,29 +3,29 @@ import PropTypes from 'prop-types';
 
 const GuessedWords = (props) => {
     let contents
-    if (props.guessedWord.length === 0) {
+    if (props.guessedWords.length === 0) {
         contents = (
-            <span>
+            <span data-test="guess-instructions">
                 Try to guess the secret word!
             </span>
         );
     } else {
-        const guessedWordsRows = props.guessedWordsRows.map((word, index) => {
-            <tr data-test="guessed-word" key={index}>
-                <td>{word.guessedWord}</td>
-                <td>{ word.letterMatchCount}</td>
-
+        const guessedWordsRows = props.guessedWords.map(word => {
+            <tr data-test="guessed-word" key={word.guessedWord}>
+                <td>{ word.guessedWord }????</td>
+                <td>{ word.letterMatchCount }</td>
             </tr>
         });
+        console.log("guessedWordsRows: ", props.guessedWords.map);
         contents = (
             <div data-test="guessed-words">
                 <h3>Guessed Words</h3>
-                <table>
-                    <thead>
+                <table className="table table-sm">
+                    <thead className="thead-light">
                         <tr><th>Guess</th><th>Matching Letters</th></tr>
                     </thead>
                     <tbody>
-                        { guessedWordsRows}
+                        { guessedWordsRows }
                     </tbody>
                 </table>
             </div>
