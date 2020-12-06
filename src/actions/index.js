799bb471a27280fcd641e0a25cf532b7382ff209
“arisa-hirata” <arisa.pirata@gmail.com>
2020-12-06 15:52:11 -0800

    set up guessWords tests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,10 @@
 export const actionTypes = {
     CORRECT_GUESS: 'CORRECT_GUESS',
+    GUESS_WORD: 'GUESSED_WORD',
 };
 
-export function correctGuess() {
-    return { type: actionTypes.CORRECT_GUESS };
-}
+export const guessWord = (guessWord) => {
+    return function (displatch, getState) {
+
+    };
+};
