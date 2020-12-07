6b67e62bb441be69b081e6c118ae789b9c861de2
“arisa-hirata” <arisa.pirata@gmail.com>
2020-12-06 19:33:27 -0800

    add getSecretWord action creator on App mount

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import Congrats from './Congrats';
 import Input from './Input';
 import { getSecretWord } from './actions';
 
-class App extends Component {
+class UnconnectedApp extends Component {
+  componentDidMount() {
+    this.props.getSecretWord();
+  }
   render() {
     return (
       <div className="container">
@@ -25,4 +28,4 @@ const mapStateToProps = (state) => {
   return { success, guessedWords, secretWord };
 }
 
-export default connect(mapStateToProps, { getSecretWord })(App);
+export default connect(mapStateToProps, { getSecretWord })(UnconnectedApp);
--- src/App.test.js
+++ src/App.test.js
@@ -40,3 +40,25 @@ describe('redux properties', () => {
     expect(getSecretWordProp).toBeInstanceOf(Function);
   });
 });
+
+test('`getSecretWord` runs on App mount', () => {
+  const getSecretWordMock = jest.fn();
+
+  const props = {
+    getSecretWord: getSecretWordMock,
+    success: false,
+    guessedWords: [],
+  }
+
+  // set up app component with getSecretWordMock as the getSecretWord prop
+  const wrapper = shallow(<UnconnectedApp {...props} />);
+
+  // run lifecycle method
+  wrapper.instance().componentDidMount();
+
+  // check to see if mock ran
+  const getSecretWordCallCount = getSecretWordMock.mock.calls.length;
+
+  expect(getSecretWordCallCount).toBe(1);
+
+});
